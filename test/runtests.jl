36a5ade4ba3c198664b05d07e3a4cc29871d1434
brendanjohnharris <brendanjohnharris@gmail.com>
2022-11-09 10:56:04 +1100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using TimeseriesSurrogates, CairoMakie
 using DelimitedFiles
 
 @testset "AperiodicSurrogates.jl" begin
-    x = readdlm("test/test.csv") |> vec
+    x = readdlm(joinpath(dirname(@__FILE__), "test.csv")) |> vec
     fs = 1250.0
     s = surrogate(x, AP(fs))
     fig = surroplot(x, s; resolution=(720, 1080), cx=:cornflowerblue, cs=:crimson)
     xlims!(fig.content[1], (0, 2000))
     xlims!(fig.content[3], (0, 0.2))
     ylims!(fig.content[3], (1e-9, nothing))
-    save("test/test.png", fig)
+    save(joinpath(dirname(@__FILE__), "test.png"), fig)
 end
