0eaee2d927316ab9181765f597115e645cae996a
brendanjohnharris <brendanjohnharris@gmail.com>
2022-11-08 22:46:03 +1100

    Update defaults

--- src/AperiodicSurrogates.jl
+++ src/AperiodicSurrogates.jl
@@ -11,13 +11,18 @@ using PyFOOOF.PyCall
 @reexport using TimeseriesSurrogates
 export Aperiodic, AP
 
-function fooof(f, res::AbstractVector; freqrange=[10.0, 300.0])
+function fooof(f, res::AbstractVector; freqrange=[1.0, 300.0])
     freqrange = py"[$(first(freqrange)), $(last(freqrange))]"o
     f = collect(f)
     spectrum = collect(res)
     fm = PyFOOOF.FOOOF(peak_width_limits=py"[0.5, 20.0]"o, max_n_peaks=4, aperiodic_mode="knee") # Maybe relax these in the future?
     fm.add_data(f, spectrum, freqrange)
     fm.fit()
+    # if fm.aperiodic_params_[2] < 0.0
+    #     fm = PyFOOOF.FOOOF(peak_width_limits=py"[0.5, 20.0]"o, max_n_peaks=4, aperiodic_mode="fixed")
+    #     fm.add_data(f, spectrum, freqrange)
+    #     fm.fit()
+    # end
     return fm
 end
 
@@ -25,7 +30,8 @@ function aperiodicfit(f, res::AbstractVector; kwargs...)
     fm = fooof(f, res; kwargs...)
     # * The aperiodic model, as described in doi.org/10.1038/s41593-020-00744-x
     b, k, χ = fm.aperiodic_params_
-    k = max(k, 0.0) # A negative knee generally leads to bad values
+    k = max(k, 0.01)
+    # b, k, χ = length(ps) ==3 ? ps : (ps[1], 0.0, ps[2])
     L = f -> 10.0.^(b - log10(k + (f)^χ))
 end
 
@@ -39,7 +45,7 @@ struct Aperiodic <: Surrogate
     fs::Number
     freqrange
 end
-Aperiodic(fs) = Aperiodic(fs, (10, 300))
+Aperiodic(fs) = Aperiodic(fs, (1, 300))
 Aperiodic(fs, freqrange::AbstractInterval, args...) = Aperiodic(fs, freqrange|>extrema)
 const AP = Aperiodic
 
@@ -59,6 +65,7 @@ function TimeseriesSurrogates.surrogenerator(x::AbstractVector, ap::Aperiodic, r
     # * Then, fit the aperiodic model to the power spectrum and calculate its Fourier coefficients
     fm = aperiodicfit(f, _r.^2, freqrange=ap.freqrange)
     r = f .|> fm .|> sqrt
+    r[f .< minimum(ap.freqrange)] = _r[f .< minimum(ap.freqrange)] # Ensures no funky behaviour outside of the fit bounds
 
     # * Scale the coefficients so the surrogate has the same energy as the original time series
     r = r.*sum(_r)./sum(r)
